d47ce767fa91b4b49081dee7ce31bb5dad01ba88
Jan Kaul <jankaul@mailbox.org>
2023-08-07 10:31:26 +0200

    implement schema

--- src/messages.rs
+++ src/messages.rs
@@ -4,6 +4,7 @@ use serde_json::Value;
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(tag = "type", rename_all = "UPPERCASE")]
 pub enum Message {
+    Schema(Schema),
     Record(Record),
 }
 
@@ -14,11 +15,19 @@ pub struct Record {
     time_extracted: Option<String>,
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Schema {
+    stream: String,
+    schema: Value,
+    key_properties: Vec<String>,
+    bookmark_properties: Option<Vec<String>>,
+}
+
 #[cfg(test)]
 pub mod tests {
     use serde_json::json;
 
-    use crate::messages::{Message, Record};
+    use crate::messages::{Message, Record, Schema};
 
     #[test]
     fn test_record() {
@@ -33,5 +42,20 @@ pub mod tests {
             time_extracted: None,
         });
         assert_eq!(record, expected);
+    }
+
+    #[test]
+    fn test_schema() {
+        let input = r#"{"type": "SCHEMA", "stream": "users", "key_properties": ["id"], "schema": {"required": ["id"], "type": "object", "properties": {"id": {"type": "integer"}}}}"#;
+
+        let schema: Message = serde_json::from_str(input).unwrap();
+
+        let expected = Message::Schema(Schema {
+            stream: "users".to_string(),
+            key_properties: vec!["id".to_string()],
+            schema: json!({"required": ["id"], "type": "object", "properties": {"id": {"type": "integer"}}}),
+            bookmark_properties: None,
+        });
+        assert_eq!(schema, expected);
     }
 }
